603266d8d459192fe2c80d85b1040599ae9ce246
Rafal <rafalk@ecom.house>
2025-04-18 08:56:24 +0200

    Add constans

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,3 +1,18 @@
-export const ERROR = "Wystąpił błąd z local storage, spróbuj ponownie.";
+export const ERROR = "An error occurred with local storage. Please try again.";
 
-export const STORAGE_NAME = "candidates";
+export const LOCAL_STORAGE_CANDIDATES_KEY = "HIRE_FLOW_APP_CANDIDATES";
+export const LOCAL_STORAGE_NOTES_KEY = "HIRE_FLOW_APP_NOTES";
+export const LOCAL_STORAGE_CONFIG_KEY = "HIRE_FLOW_APP_CONFIG";
+
+export const CONTACT_COLORS = [
+  "#E53935",
+  "#D81B60",
+  "#8E24AA",
+  "#5E35B1",
+  "#3949AB",
+  "#1E88E5",
+  "#00897B",
+  "#43A047",
+  "#FB8C00",
+  "#6D4C41",
+];
